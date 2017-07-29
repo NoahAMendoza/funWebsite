2348971cd1169caaee2530af5b83da7f85c0823f
Noah Mendoza <NoahMendoza622@gmail.com>
2017-07-29 18:47:04 -0500

    lambda python script

---
+++ serverless-fun/createUser.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import uuid
+
+def lambda_handler(event, context):
+    
+    recordId = str(uuid.uuid4())
+    username = event["username"]
+
+    print('Creating new user:  ' username)
+
+    #Creating new record in DynamoDB table
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DB_TABLE_NAME'])
+    table.put_item(
+        Item={
+            'id' : recordId,
+            'text' : text,
+            'voice' : voice,
+            'status' : 'PROCESSING'
+        }
+    )
+    
+    #Sending notification about new post to SNS
+    client = boto3.client('sns')
+    client.publish(
+        TopicArn = os.environ['SNS_TOPIC'],
+        Message = recordId
+    )
+    
+    return recordId
